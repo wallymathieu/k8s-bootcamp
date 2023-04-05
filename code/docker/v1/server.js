f0116ab09a6968d9c91e94f32f8f4a32b7f2aa28
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2023-04-05 18:40:28 +0200

    port

--- code/docker/v1/server.js
+++ code/docker/v1/server.js
@@ -12,7 +12,7 @@ var handleRequest = function(request, response) {
   console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());
 }
 var www = http.createServer(handleRequest);
-www.listen(8081,function () {
+www.listen(8080,function () {
     startTime = new Date();;
     host = process.env.HOSTNAME;
     console.log ("Kubernetes Bootcamp App Started At:",startTime, "| Running On: " ,host, "\n" );
